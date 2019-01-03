55fffaf688e435703ccaf08091776a2ab37d894e
Jasper <bobmugura@gmail.com>
2019-01-02 20:19:37 -0500

    added dynamic and nested routing

--- routers/src/components/Topics.js
+++ routers/src/components/Topics.js
@@ -1,5 +1,34 @@
 import React from 'react'
+import { Link, Route } from 'react-router-dom'
 
-export default function Topics() {
-  return <div>TOPICS</div>
+function Topic(props) {
+  return <h3>{props.match.params.topicId}</h3>
+}
+
+export default function Topics(props) {
+  const match = props.match;
+  // console.log('match.url', match.url)
+  return (
+    <div>
+      <h2>Topics</h2>
+      <ul>
+        <li>
+          <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>Props vs State</Link>
+        </li>
+      </ul>
+
+      <hr />
+
+      <Route path={`${match.path}/:topicId`} component={Topic} />
+      <Route exact path={match.path} render={() => {
+        return <h3>Please Select a topic</h3>
+      }} />
+    </div>
+  )
 }
